eb8a5eea1d6517b65c56eb4c070b5e864c1f25b7
koytomraz <97357117+koytomraz@users.noreply.github.com>
2026-04-13 12:50:01 +0300

    Update parser.js
    Added: _parseArrayIndexing, Array assignment, array primary

--- parser.js
+++ parser.js
@@ -229,6 +229,30 @@ class Parser {
     this._expect(TokenType.RBRACE);
     return AST.BlockStatement(body);
   }
+    
+  _parseArrayIndexing(arr) {
+    // Getting the values
+	  // arr is either Identifier or ArrayLiteral
+	  if (this._check(TokenType.LSPAREN)){
+	    const indexes = [];
+	    while (this._check(TokenType.LSPAREN)){
+	      this._advance();
+		
+	      const index = this._parseExpression(); //stupid decision, change later
+		
+	      this._expect(TokenType.RSPAREN);
+		
+	      //if (indent.type !== 'Identifier') {
+          //  throw new SyntaxError('[Parser] Indexing only works for arrays or lists');
+          //}
+	  
+	      indexes.push(index)
+	    }
+	    return AST.IndexExpression(arr, indexes)
+	  } else {
+        return arr
+	  }
+  }
 
   // ── Expressions (operator-precedence climb) ───────────────────────────────
   //
@@ -242,14 +266,15 @@ class Parser {
 
   _parseAssignment() {
     const left = this._parseLogicalOr();
-
     if (this._check(TokenType.ASSIGN)) {
       this._advance();
-      if (left.type !== 'Identifier') {
+      // Check if it's an Identifier or IndexExpression
+      if ((left.type !== 'Identifier') && (left.type !== 'IndexExpression')) {
         throw new SyntaxError('[Parser] Invalid assignment target: left-hand side must be a variable name');
       }
       const value = this._parseAssignment(); // right-associative
-      return AST.AssignmentExpression(left.name, value);
+	  if (left.type === 'Identifier') return AST.AssignmentExpression(left.name, value);
+	  if (left.type === 'IndexExpression') return AST.ArrayAssignment(left.array.name, left.indexes, value);
     }
 
     return left;
@@ -392,6 +417,22 @@ class Parser {
     if (tok.type === TokenType.IDENTIFIER) {
       this._advance();
       return AST.Identifier(tok.value);
+    }
+
+    // Array expression
+    if (tok.type === TokenType.LSPAREN) {
+      this._advance();
+	    const arr = [];
+	    if (!this._check(TokenType.RSPAREN)) {
+          arr.push(this._parsePrimary());
+          while (this._match(TokenType.COMMA)) {
+            arr.push(this._parsePrimary());
+          }
+        }
+        this._expect(TokenType.RSPAREN);
+	  
+	    const arrayLiteral = AST.ArrayLiteral(arr);
+	    return this._parseArrayIndexing(arrayLiteral);
     }
 
     // Grouped expression  (expr)
